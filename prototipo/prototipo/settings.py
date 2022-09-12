dbc5526b21577e60f82e0b083ed6751803d0074a
AdriSC <adrsc96@gmail.com>
2022-09-12 14:47:07 +0200

    fixes

--- prototipo/prototipo/settings.py
+++ prototipo/prototipo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@=*!k*w4da3(hoic+f%1$_=2c2t1%!6o-new&f8484efq^9_s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
